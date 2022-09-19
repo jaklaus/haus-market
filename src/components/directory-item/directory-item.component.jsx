932807bb8c2cec4380e4d3aa500f4ad3be0264ae
Joel Klaus <jaklaus0101@gmail.com>
2022-09-19 14:16:45 -0500

    fixing routing on homepage

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,9 +1,13 @@
 import './directory-item.styles.scss';
+import { useNavigate } from 'react-router-dom';
 
 const DirectoryItem = ({category}) => {
-    const {id, title, imageURL} = category;
+    const {id, title, imageURL, route} = category;
+    const navigate = useNavigate();
+    const onNavigationHandler = () => navigate(route);
+
     return (
-        <div key={id} className="directory-item-container">
+        <div key={id} className="directory-item-container" onClick={onNavigationHandler}>
             <div className="directory-body-container">
                 <h1>{title}</h1>
                 <p>Shop Now</p>
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,8 +1,39 @@
 import { StyledDirectoryContainer } from './directory.styles';
 import DirectoryItem from '../directory-item/directory-item.component';
+const categories = [
+    {
+        id: 1,
+        title: "Hats",
+        imageURL: 'https://images.unsplash.com/photo-1575428652377-a2d80e2277fc?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340',
+        route: 'shop/hats'
+    },
+    {
+        id: 2, 
+        title: "Sneakers", 
+        imageURL: 'https://images.unsplash.com/photo-1549298916-b41d501d3772?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1412',
+        route: 'shop/sneakers'
+    },
+    {
+        id: 3, 
+        title: "Jackets", 
+        imageURL: 'https://images.unsplash.com/photo-1495105787522-5334e3ffa0ef?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740',
+        route: 'shop/jackets'
+    },
+    {
+        id: 4, 
+        title: "Men's", 
+        imageURL: 'https://images.unsplash.com/photo-1563183927-071ac1d9aef6?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1272',
+        route: 'shop/mens'
+    },
+    {
+        id: 5, 
+        title: "Women's", 
+        imageURL: 'https://images.unsplash.com/photo-1503342217505-b0a15ec3261c?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740',
+        route: 'shop/womens'
+    },
+];
 
-const Directory = ({categories}) => {
-    
+const Directory = () => {
     return (
         <StyledDirectoryContainer>
             {categories.map((category) => {
